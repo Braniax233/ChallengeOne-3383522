b6296139fa0f2e8d36d02c8f6c46394cda386371
Braniax233 <168475067+Braniax233@users.noreply.github.com>
2025-06-10 23:34:58 +0000

    Add files via upload
    assignment

---
+++ ChallengeOne-3383522.js
@@ -0,0 +1,42 @@
+class TrieNode {
+    constructor() {
+      this.children = {};
+      this.end = false;
+    }
+  }
+  
+  class Trie {
+    constructor() {
+      this.root = new TrieNode();
+    }
+  
+    insert(word) {
+      let node = this.root;
+      for (let ch of word) {
+        if (!node.children[ch]) {
+          node.children[ch] = new TrieNode();
+        }
+        node = node.children[ch];
+      }
+      node.end = true;
+    }
+  
+    search(word) {
+      let node = this.root;
+      for (let ch of word) {
+        if (!node.children[ch]) return false;
+        node = node.children[ch];
+      }
+      return node.end;
+    }
+  
+    startsWith(prefix) {
+      let node = this.root;
+      for (let ch of prefix) {
+        if (!node.children[ch]) return false;
+        node = node.children[ch];
+      }
+      return true;
+    }
+  }
+  
